3c59474713e8359a28b0ab1822a01f87a88b9d9d
Sama Seijuuro Jin Sakai Shin <rehda.p6@outlook.fr>
2025-09-05 00:14:27 +0200

    Second Push

---
+++ SwiftUI Apple Frameworks/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  SwiftUI Apple Frameworks
+//
+//  Created by Seijuuro Jin Sakai Shin on 05/09/2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
