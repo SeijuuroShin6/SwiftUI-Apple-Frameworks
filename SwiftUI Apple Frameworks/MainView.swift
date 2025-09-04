16bcba2415ca153ca68774414bb07149ac3be622
Sama Seijuuro Jin Sakai Shin <rehda.p6@outlook.fr>
2025-09-05 00:58:19 +0200

    Second Commit

--- SwiftUI Apple Frameworks/MainView.swift
+++ SwiftUI Apple Frameworks/MainView.swift
@@ -9,10 +9,49 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .leading) {
+            Color(.black)
+                .ignoresSafeArea()
+            //           VStack() {
+            //            Text("🍎 Frameworks")
+            //          .foregroundColor(.white)
+            //        .font(.system(size: 30, weight: .bold, design: .default))
+            //  }
+            
+            VStack(spacing: 20) {
+                gridContentView(appImage: "app-clip", nameApp: "App Clips")
+                gridContentView(appImage: "arkit", nameApp: "ARKit")
+                gridContentView(appImage: "carplay", nameApp: "Car Play")
+
+            }
+        }
+    }
+    
+    
+
+    
+    
+    struct gridContentView: View {
+        
+        var appImage: String
+        var nameApp: String
+        
+        var body: some View {
+            VStack {
+                Image(appImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 100, height: 100)
+                
+                Text(nameApp)
+                    .foregroundColor(.white)
+                    .font(.system(size: 20, weight: .bold))
+            }
+        }
     }
 }
 
+
 #Preview {
-    MainView()
+ MainView()
 }
