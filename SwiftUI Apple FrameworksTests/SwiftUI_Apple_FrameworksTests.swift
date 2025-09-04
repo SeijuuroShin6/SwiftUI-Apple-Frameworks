2826c9713996c5242be74dc3d313ddb0451410d9
Sama Seijuuro Jin Sakai Shin <rehda.p6@outlook.fr>
2025-09-04 23:32:14 +0200

    First Commit

---
+++ SwiftUI Apple FrameworksTests/SwiftUI_Apple_FrameworksTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Apple_FrameworksTests.swift
+//  SwiftUI Apple FrameworksTests
+//
+//  Created by Seijuuro Jin Sakai Shin on 04/09/2025.
+//
+
+import Testing
+@testable import SwiftUI_Apple_Frameworks
+
+struct SwiftUI_Apple_FrameworksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
